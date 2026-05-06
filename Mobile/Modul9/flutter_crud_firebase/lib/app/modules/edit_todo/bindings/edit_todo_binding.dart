72941160849cc38d73c7f09e7eba93ad40b24c8a
NarCobahtml <aryaganzz09@gmail.com>
2026-05-06 21:42:28 +0700

    adding module 9

---
+++ Mobile/Modul9/flutter_crud_firebase/lib/app/modules/edit_todo/bindings/edit_todo_binding.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+import '../controllers/edit_todo_controller.dart';
+
+class EditTodoBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<EditTodoController>(() => EditTodoController());
+  }
+}
